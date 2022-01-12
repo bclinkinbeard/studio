99dffb9ad247a56ea807e837dcfd5d7364fc34fb
Esther Weon <esther.s.weon@gmail.com>
2022-01-12 11:50:28 -0800

    Add panel settings for displaying axes labels; fix floating legend z-index; fix chart centering; do not show scroll unless overflow

--- packages/studio-base/src/panels/StateTransitions/index.tsx
+++ packages/studio-base/src/panels/StateTransitions/index.tsx
@@ -386,6 +386,7 @@ const StateTransitions = React.memo(function StateTransitions(props: Props) {
           <TimeBasedChart
             zoom
             isSynced
+            showXAxisLabels
             width={width ?? 0}
             height={height}
             data={data}
